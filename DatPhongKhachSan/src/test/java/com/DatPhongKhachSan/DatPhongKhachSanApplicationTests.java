dd40606c3e2745f20ca10114280692e3b5a4072b
ThienTanMk <125727960+ThienTanMk@users.noreply.github.com>
2024-01-30 01:11:38 +0700

    create demo web by SpringBoot
    demo web by SpringBoot with Maven and just have entity now

---
+++ DatPhongKhachSan/src/test/java/com/DatPhongKhachSan/DatPhongKhachSanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DatPhongKhachSan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatPhongKhachSanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

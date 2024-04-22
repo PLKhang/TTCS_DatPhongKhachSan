f83499c3fc0518c0ad0afe0d27d6ec05c2c65d76
PLKhang <plkhang090403@gmail.com>
2024-04-23 00:05:01 +0700

    add DTO files'

---
+++ DatPhongKhachSan/src/main/java/com/DatPhongKhachSan/DTO/PhongDTO.java
@@ -0,0 +1,16 @@
+package com.DatPhongKhachSan.DTO;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PhongDTO {
+    private String Maphong;
+    private String MaHP;
+    private int SoPhong;
+    private int SoTang;
+    private String TrangThai;
+}
+

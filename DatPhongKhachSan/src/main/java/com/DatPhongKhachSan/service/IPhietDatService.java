633ea5a409bb0e9496a4f8a51458d9f08a2e6803
ThienTanMk <125727960+ThienTanMk@users.noreply.github.com>
2024-05-08 23:06:46 +0700

    Add Service File: CTPD, PD

---
+++ DatPhongKhachSan/src/main/java/com/DatPhongKhachSan/service/IPhietDatService.java
@@ -0,0 +1,26 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.PhieuDat;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IPhietDatService {
+    List<PhieuDat> findAll();
+
+    List<PhieuDat> findAllById(Iterable<String> strings);
+
+
+    Optional<PhieuDat> findById(String s);
+
+    long count();
+
+    void deleteById(String s);
+
+    void delete(PhieuDat entity);
+
+    void deleteAllById(Iterable<? extends String> strings);
+    List<PhieuDat> findByKhachHangCCCD(String khachHangCCCD);
+    void deleteAll();
+    PhieuDat save(PhieuDat phieuDat);
+}

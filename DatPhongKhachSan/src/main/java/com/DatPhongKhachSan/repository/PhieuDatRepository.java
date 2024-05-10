97e47befeedfcfbaff54b811093361b791fd9190
ThienTanMk <125727960+ThienTanMk@users.noreply.github.com>
2024-05-11 00:23:31 +0700

    Add file Repository: PD, CTPD, KhachHang

---
+++ DatPhongKhachSan/src/main/java/com/DatPhongKhachSan/repository/PhieuDatRepository.java
@@ -0,0 +1,14 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.PhieuDat;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PhieuDatRepository extends JpaRepository<PhieuDat,String> {
+    List<PhieuDat> findAll();
+    PhieuDat save(PhieuDat phieuDat);
+    List<PhieuDat> findByKhachHangCCCD(String khachHangCCCD);
+}
